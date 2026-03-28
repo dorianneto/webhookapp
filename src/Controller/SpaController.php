6add73ab9c68b8c35070262d8d9a24d643b38968
Dorian Neto <doriansampaioneto@gmail.com>
2026-03-28 10:45:31 -0230

    fix spa controller to display static assets through Symfony

--- src/Controller/SpaController.php
+++ src/Controller/SpaController.php
@@ -2,15 +2,16 @@
 
 namespace App\Controller;
 
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/{reactRouting}', name: 'spa', requirements: ['reactRouting' => '^(?!api|in).*'], priority: -10)]
 class SpaController
 {
-    public function __invoke(string $kernel_project_dir): Response
+    public function __invoke(#[Autowire('%kernel.project_dir%')] string $projectDir): Response
     {
-        $html = file_get_contents($kernel_project_dir . '/public/build/index.html');
+        $html = file_get_contents($projectDir . '/public/build/index.html');
         return new Response($html, 200, ['Content-Type' => 'text/html']);
     }
 }
